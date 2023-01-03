8b35bcf00f4801e7c56db7c94386ee4860e926f5
Ezen <Ezen@Ezen>
2023-01-03 15:01:15 +0900

    commit

--- SpringProject/src/main/java/com/edu/member/MemberDAO/MemberDAO.java
+++ SpringProject/src/main/java/com/edu/member/MemberDAO/MemberDAO.java
@@ -5,23 +5,11 @@ import org.springframework.dao.DataAccessException;
 import com.edu.member.MemberDTO;
 
 public interface MemberDAO {
-	public MemberDTO loginByID(MemberDTO memberDTO) throws DataAccessException;
+	public MemberDTO loginByID(MemberDTO memberDTO)
+			throws DataAccessException;
 	
 	
 
-<mapper namespalce - com.edu.member>
-<resultMap id="memberResult" type="com.edu.member.dto.MemberDTO">
-	<result property="id" column="id"/>
-	<result property="pwd" column="pwd"/>
-	<result property="id" column="name"/>
-	<result property="id" column="email"/>
-	<result property="id" column="joinDate"/>
 
-</resultMap>
-
-<selelct id="loginByID" parameterType="">
-</mapper>	
-	
-	
 	
 }
